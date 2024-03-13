127386f1fb58a1e566006616b6fd98f37847707f
Paula Ferreira <psilf12@gmail.com>
2024-03-13 10:58:30 +0000

    first

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='golden_fish',
+	version='0.1.0',
+   	author='Paula Ferreira',
+   	author_email='psilf12@gmail.com',
+   	packages=['golden_fish',],
+   	#scripts=['bin/script1','bin/script2'],
+   	#url='http://pypi.python.org/pypi/PackageName/',
+   	license='LICENSE.txt',
+   	description='Fisher Forecast plots.',
+  	#long_description=open('README.txt').read(),
+  	install_requires=[
+	"astropy",
+	"numpy",
+	'scipy',
+	'pandas',
+	'matplotlib.pyplot'],)
