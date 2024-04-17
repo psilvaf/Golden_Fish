158e3a82b717bdc21ecea39f5f8c2e911ca2b277
Paula Ferreira <psilf12@gmail.com>
2024-04-17 10:23:24 +0100

    17april

--- build/lib/golden_fish/analyse_areas.py
+++ build/lib/golden_fish/analyse_areas.py
@@ -1,11 +1,7 @@
 import numpy as np
 
 def estimate_ellipse_parameters(x_points, y_points):
-    # Estimate ellipse parameters using curve fitting or other methods
-    # For example, you can use the least squares method to fit an ellipse equation
-    # to the given points. You can use libraries like scipy.optimize.curve_fit.
-
-    # Here's a simple example assuming you have the semi-major and semi-minor axes.
+    
     semi_major_axis = max(x_points) - min(x_points)
     semi_minor_axis = max(y_points) - min(y_points)
 
@@ -13,3 +9,11 @@ def estimate_ellipse_parameters(x_points, y_points):
 
 def compute_ellipse_area(semi_major_axis, semi_minor_axis):
     return np.pi * semi_major_axis * semi_minor_axis
+    
+def area_values(ellip_x,ellip_y):
+    '''
+    Compute ellipse areas from parameters pairs.
+    '''
+    semi_major_axis, semi_minor_axis = estimate_ellipse_parameters(ellip_x,ellip_y)
+    area = compute_ellipse_area(semi_major_axis, semi_minor_axis)
+    return area
